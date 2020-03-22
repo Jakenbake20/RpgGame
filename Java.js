21920c6ed5b6ee90fe37e744540da1a18ac332d8
Jakenbake <jacob_wheat@yahoo.com>
2020-03-22 13:56:47 -0600

    Created initial neccassary files and directories to start this project.

---
+++ Java.js
@@ -0,0 +1,20 @@
+// alert ("Metal Gear RPG"); {
+//     console.log ("alert message");
+// }
+// var's for character cards/buttons
+//Each character should be an object with assigned attributes ex. Max health, damage, attack.
+// Need a reset button
+//
+
+var solidSnake = {
+    name: "Solid Snake",
+    healthPoints: 200,
+    attackPower: 40,
+    CounterAttackPower: 25
+
+    var iiquidSnake = solidSnake[name];
+    console.log(liquidSnake);
+
+};
+
+
