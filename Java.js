595b4dac50ae2495b4f4e372bb05405e4eb8cd88
Jakenbake <jacob_wheat@yahoo.com>
2020-03-24 17:14:19 -0600

    added more Pictures for thumbnails and added div containers within my current divs.

--- Java.js
+++ Java.js
@@ -9,10 +9,49 @@
 var solidSnake = {
     name: "Solid Snake",
     healthPoints: 200,
-    attackPower: 40,
+    attackPower: 50,
     CounterAttackPower: 25
-
-
 };
-
+var liquidSnake = {
+    name: "Liquid Snake",
+    healthPoints: 200,
+    attackPower: 50,
+    CounterAttackPower: 25
+};
+var revolverOcelot = {
+    name: "Revolver Ocelot",
+    healthPoints: 150,
+    attackPower: 30,
+    CounterAttackPower: 35
+};
+var bigBoss = {
+    name: "Big Boss",
+    healthPoints: 200,
+    attackPower: 60,
+    CounterAttackPower: 20
+};
+var otacon = {
+    name: "Otacon",
+    healthPoints: 100,
+    attackPower: 15,
+    CounterAttackPower: 5
+};
+var sniperWolf = {
+    name: "Sniper Wolf",
+    healthPoints: 120,
+    attackPower: 45,
+    CounterAttackPower: 35
+};
+var raiden = {
+    name: "Raiden",
+    healthPoints: 120,
+    attackPower: 60,
+    CounterAttackPower: 50
+};
+var grayFox = {
+    name: "Gray Fox",
+    healthPoints: 200,
+    attackPower: 40,
+    CounterAttackPower: 35
+};
 
