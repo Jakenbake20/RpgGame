7d08b808464e719b922b0cd32e65b598e949db31
Jakenbake <jacob_wheat@yahoo.com>
2020-03-24 23:41:04 -0600

    Changed format of the game. Added buttons and div containers using bootstrap format. Added pic for background and resized all pics to fit better as thumbnails. Names stopped populating in js.

--- Java.js
+++ Java.js
@@ -1,57 +1,153 @@
-// alert ("Metal Gear RPG"); {
-//     console.log ("alert message");
-// }
-// var's for character cards/buttons
-//Each character should be an object with assigned attributes ex. Max health, damage, attack.
-// Need a reset button
-//Need an alert for a win/loss
+$(document).ready(function() {
+	var myChar, opponentChar, choices, enemyArray, haveCharacter, haveAttacker, numEnemies, rounds;	//Set Global Variables
+	var wins = 0;
+	var loses = 0;
 
-var solidSnake = {
-    name: "Solid Snake",
-    healthPoints: 200,
-    attackPower: 50,
-    CounterAttackPower: 25
-};
-var liquidSnake = {
-    name: "Liquid Snake",
-    healthPoints: 200,
-    attackPower: 50,
-    CounterAttackPower: 25
-};
-var revolverOcelot = {
-    name: "Revolver Ocelot",
-    healthPoints: 150,
-    attackPower: 30,
-    CounterAttackPower: 35
-};
-var bigBoss = {
-    name: "Big Boss",
-    healthPoints: 200,
-    attackPower: 60,
-    CounterAttackPower: 20
-};
-var otacon = {
-    name: "Otacon",
-    healthPoints: 100,
-    attackPower: 15,
-    CounterAttackPower: 5
-};
-var sniperWolf = {
-    name: "Sniper Wolf",
-    healthPoints: 120,
-    attackPower: 45,
-    CounterAttackPower: 35
-};
-var raiden = {
-    name: "Raiden",
-    healthPoints: 120,
-    attackPower: 60,
-    CounterAttackPower: 50
-};
-var grayFox = {
-    name: "Gray Fox",
-    healthPoints: 200,
-    attackPower: 40,
-    CounterAttackPower: 35
-};
+	function varSet() {		//Sets all of the variable values
+		myChar;
+		opponentChar;
 
+		choices = [];
+		enemyArray = [ {
+			id: 0,
+			name: "SolidSnake",
+			pic: '../Images/SolidSnake.jpg',
+			hitPoints: 150,
+			attackPower: 5
+		}, {
+			id: 1,
+			name: "LiquidSnake",
+			pic: '../Images/LiquidSnake.jpg',
+			hitPoints: 100,
+			attackPower: 25 		
+		}, {
+			id: 2,
+			name: "RevolverOcelot",
+			pic: '../Images/RevolverOcelot.jpg',
+			hitPoints: 120,
+			attackPower: 19 
+		}, {
+			id: 3,
+			name: "Raiden",
+			pic: '../Images/Raiden.jpg',
+			hitPoints: 140,
+			attackPower: 9 
+		} ];
+
+		haveCharacter = false;
+		haveAttacker = false;
+		numEnemies = 3;
+		rounds = 7;
+
+		for(var i = 0; i < enemyArray.length; i++) {
+			choices += "<div id=" + enemyArray[i].id + " class='btn character text-center' value=" + enemyArray[i].id +
+			"><img class='houses' src=" + enemyArray[i].pic + " alt=" + enemyArray[i].name + "><br> HP: " + enemyArray[i].hitPoints +
+			"<br> AP: " + enemyArray[i].attackPower + " </div>";
+		}
+
+		$("#picking").html(choices);
+		$("#todo").html("Click on your hero!");
+
+		$('.hero').remove();
+		$('.fighting').remove();
+		$('#whathappens').html("");
+
+		attachCharacterOnClick();
+	}
+
+	function printCharacters() {
+		var hero = "<div id=" + enemyArray[myChar].id + " class='btn character text-center hero' value=" + enemyArray[myChar].id +
+			"><img class='houses' src=" + enemyArray[myChar].pic + " alt=" + enemyArray[myChar].name + "><br> HP: " + enemyArray[myChar].hitPoints +
+			"<br> AP: " + enemyArray[myChar].attackPower + " </div>";
+		var badguy = "<div id=" + enemyArray[opponentChar].id + " class='btn character text-center fighting' value=" + enemyArray[opponentChar].id +
+			"><img class='houses' src=" + enemyArray[opponentChar].pic + " alt=" + enemyArray[opponentChar].name + "><br> HP: " + enemyArray[opponentChar].hitPoints +
+			"<br> AP: " + enemyArray[opponentChar].attackPower + " </div>";
+		$('#myguy').html(hero);
+		$('#enemy').html(badguy);
+	}
+
+	function whatHappens() {
+		var description = "You attack " + enemyArray[opponentChar].name + " for " + enemyArray[myChar].attackPower + " damage!<br>" +
+			enemyArray[opponentChar].name + " counter attacks for " + enemyArray[opponentChar].attackPower + " damage!<br>" +
+			"Your attack power has increased by " + rounds + "!";
+		$('#whathappens').html(description);
+	}
+
+	function attachCharacterOnClick() {
+		$('.character').on("click", function(){
+			if(!haveCharacter) {	//Picking your character
+				myChar = $(this).attr('id');
+				$("#myguy").append(this);
+				$(this).addClass("hero");
+
+				haveCharacter = true;
+				$('#whathappens').html("");
+				$("#todo").html("Choose your opponent!");
+			}
+			//You have a character and you're picking your opponent
+			else if(!haveAttacker && haveCharacter && myChar !== $(this).attr('id')) {	
+				opponentChar = $(this).attr('id');
+				$("#enemy").append(this);
+				$(this).addClass("fighting");
+
+				haveAttacker = true;
+				$('#whathappens').html("");
+				$("#todo").html("Keep clicking attack to duel!");
+			}
+		});
+	}
+
+	$('#attack').on("click", function() {
+		if(!haveCharacter) {
+			$('#whathappens').html("You need to pick your house first!");
+		}
+		else if(!haveAttacker) {
+			$('#whathappens').html("Pick who you are fighting!");
+		}
+		else if(haveCharacter && haveAttacker) {
+			rounds++;
+			enemyArray[opponentChar].hitPoints  = enemyArray[opponentChar].hitPoints - enemyArray[myChar].attackPower;	//Hit Them
+			enemyArray[myChar].hitPoints = enemyArray[myChar].hitPoints - enemyArray[opponentChar].attackPower;	//Get Hit
+
+
+			if(enemyArray[opponentChar].hitPoints < 0) {
+				numEnemies--;
+				if(numEnemies > 0) {
+					$(".fighting").remove();
+					$('#whathappens').html("");
+					$("#todo").html("Who will you fight next?");
+					haveAttacker = false;
+				}
+				else {
+					whatHappens();
+					alert("You win! Keep fighting for your cause!");
+					wins++;
+					$('#winsloses').html("Victories: " + wins + "&nbsp;&nbsp;Defeats: " + loses);
+					varSet();
+				}
+				
+			}
+			else if(enemyArray[myChar].hitPoints < 0) {
+				whatHappens();
+				alert("Snake?! Snake?! SNAAAAAAKE!!!!");
+				loses++;
+				$('#winsloses').html("Victories: " + wins + "&nbsp;&nbsp;Defeats: " + loses);
+				varSet();
+			}
+			else {
+				whatHappens();
+				printCharacters();
+			}
+
+			enemyArray[myChar].attackPower = enemyArray[myChar].attackPower + rounds;	//Get Stronger
+		}
+	});
+
+	$('#restart').on("click", function(){
+		varSet();
+	});
+
+	attachCharacterOnClick();
+	varSet();
+
+});
